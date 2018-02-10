cd919ad8269e1a5170ba7bec87f9d228538c8279
Gabor Csizmadia <gaborcsiz@gmail.com>
2018-02-10 12:49:49 +0100

    update react-sortable-table

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'typeface-roboto';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'react-sortable-tree/style.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
