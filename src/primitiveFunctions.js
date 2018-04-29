a2f46a8244d3c0629c4d5f8113c3d05e72ef13d4
Gabor Csizmadia <gaborcsiz@gmail.com>
2018-04-29 20:40:46 +0200

    add more primitive functions

--- src/primitiveFunctions.js
+++ src/primitiveFunctions.js
@@ -1,10 +1,22 @@
 export default {
+    '=': {
+        description: 'Primitive function that checks if its two arguments are equal.',
+        apply: a => b => a === b
+    },
     '+': {
         description: 'Primitive function that returns the sum of two numbers.',
         apply: a => b => a + b
     },
+    '-': {
+        description: 'Primitive function that returns the first argument minus the second argument.',
+        apply: a => b => a - b
+    },
     '*': {
         description: 'Primitive function that returns the product of two numbers.',
+        apply: a => b => a * b
+    },
+    '/': {
+        description: 'Primitive function that returns the first argument divided by the second argument.',
         apply: a => b => a * b
     }
 };
