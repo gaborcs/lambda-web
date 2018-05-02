17cb6266f12fa2a73d4222ba082ced48339255ae
Gabor Csizmadia <gaborcsiz@gmail.com>
2018-05-02 23:25:08 +0200

    google analytics

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 import TouchBackend from 'react-dnd-touch-backend';
 import MultiBackend, { TouchTransition } from 'react-dnd-multi-backend';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history'
 import Ui from './Ui';
+
+const history = createBrowserHistory();
+history.listen((location, action) => {
+    window.gtag('config', window.GA_TRACKING_ID, {
+        'page_path': location.pathname
+    });
+});
 
 class App extends Component {
     render() {
-        return <Router><Ui /></Router>;
+        return <Router history={history}><Ui /></Router>;
     }
 }
 
