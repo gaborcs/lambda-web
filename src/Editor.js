14e22ac37cf401dd2cabc850708929a0a3d677cc
Gabor Csizmadia <gaborcsiz@gmail.com>
2018-05-20 23:11:23 +0200

    better support for keyboard controls in editor

--- src/Editor.js
+++ src/Editor.js
@@ -22,7 +22,8 @@ const styles = {
 class Editor extends Component {
     state = {
         input: this.props.initialInput,
-        infoOpen: false
+        infoOpen: false,
+        selectedIndex: 0
     }
 
     render = () => (
@@ -47,26 +48,54 @@ class Editor extends Component {
     handleInputChange = event => {
         let { value } = event.target;
         this.setState({
-            input: value === '.' ? lambdaChar : value
+            input: value === '.' ? lambdaChar : value,
+            selectedIndex: 0
         });
     };
 
     handleInputKeyDown = event => {
-        if (event.key === 'Enter') {
-            this.saveResult();
+        switch (event.key) {
+            case 'Enter':
+                this.saveResult();
+                return;
+            case 'ArrowDown':
+                this.setState(state => ({
+                    selectedIndex: Math.min(state.selectedIndex + 1, this.getMatchCount() - 1)
+                }));
+                event.preventDefault();
+                return;
+            case 'ArrowUp':
+                this.setState(state => ({
+                    selectedIndex: Math.max(0, state.selectedIndex - 1)
+                }));
+                event.preventDefault();
+                return;
+            default:
+                return;
         }
     };
 
     saveResult = () => {
-        let input = this.state.input;
-        if (input.startsWith(lambdaChar)) {
-            this.props.saveResult('function', input.substr(1));
-        } else if (isNaN(input)) {
-            this.props.saveResult('placeholder', input);
+        let item = this.getMatches()[this.state.selectedIndex];
+        if (item) {
+            this.props.saveResult(item.type, item.value);
         } else {
-            this.props.saveResult('number', +input);
+            let input = this.state.input;
+            if (input.startsWith(lambdaChar)) {
+                this.props.saveResult('function', input.substr(1));
+            } else if (isNaN(input)) {
+                this.props.saveResult('placeholder', input);
+            } else {
+                this.props.saveResult('number', +input);
+            }
         }
     };
+
+    getMatchCount = () => this.getMatches().length;
+
+    getMatches = () => this.props.items.filter(this.startsWithInput);
+
+    startsWithInput = item => item.name.startsWith(this.state.input);
 
     renderInputAdornment = () => (
         <InputAdornment position="end">
@@ -91,14 +120,13 @@ class Editor extends Component {
         </div>
     );
 
-    renderItems = () => this.getItems().map(this.renderItem);
-
-    getItems = () => this.props.items.filter(this.matchesSearch);
-
-    matchesSearch = item => item.name.startsWith(this.state.input);
+    renderItems = () => this.getMatches().map(this.renderItem);
 
     renderItem = (item, index) => (
-        <MenuItem key={index} onClick={() => this.props.saveResult(item.type, item.value)}>
+        <MenuItem
+            key={index}
+            selected={index === this.state.selectedIndex}
+            onClick={() => this.props.saveResult(item.type, item.value)}>
             {item.name}
         </MenuItem>
     );
