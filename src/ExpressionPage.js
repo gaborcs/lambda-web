503603d8a04253f587f5551c0a1883253b12d585
Gabor Csizmadia <gaborcsiz@gmail.com>
2018-05-27 17:48:20 +0200

    extract TreeEditor from ExpressionPage

--- src/ExpressionPage.js
+++ src/ExpressionPage.js
@@ -1,20 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Preview } from 'react-dnd-multi-backend';
 import withScrolling from 'react-dnd-scrollzone';
-import { SortableTreeWithoutDndContext as SortableTree, changeNodeAtPath, removeNodeAtPath, addNodeUnderParent, getFlatDataFromTree } from 'react-sortable-tree';
 import { withStyles } from '@material-ui/core/styles';
-import red from '@material-ui/core/colors/red';
-import indigo from '@material-ui/core/colors/indigo';
-import lightBlue from '@material-ui/core/colors/lightBlue';
-import green from '@material-ui/core/colors/green';
-import grey from '@material-ui/core/colors/grey';
 import ButtonBase from '@material-ui/core/ButtonBase';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import Chip from '@material-ui/core/Chip';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
 import Popover from '@material-ui/core/Popover';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
@@ -22,122 +12,49 @@ import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import UndoIcon from '@material-ui/icons/Undo';
 import RedoIcon from '@material-ui/icons/Redo';
 import AutoSelectInput from './AutoSelectInput';
-import NodeEditor from './NodeEditor';
 import Toolbar from './Toolbar';
 import evaluator from './evaluator';
-import primitiveFunctions from './primitiveFunctions';
-import specialForms from './specialForms';
+import TreeEditor from './TreeEditor';
 
-const chipHeight = 32;
-const minTouchTargetSize = 48;
 const lambdaChar = '\u03BB';
 
-const styles = theme => {
-    let lineColor = theme.palette.grey[700];
-    let lineColorBackground = { backgroundColor: lineColor };
-    return {
-        '@global': {
-            '.rst__lineHalfHorizontalRight::before': lineColorBackground,
-            '.rst__lineFullVertical::after': lineColorBackground,
-            '.rst__lineHalfVerticalTop::after': lineColorBackground,
-            '.rst__lineHalfVerticalBottom::after': lineColorBackground
-        },
-        root: {
-            display: 'flex',
-            flexDirection: 'column',
-            height: '100%',
-            userSelect: 'none'
-        },
-        appBarLeft: {
-            flex: '1 1 auto',
-            minWidth: 0,
-            display: 'flex'
-        },
-        name: {
-            padding: '8px 20px',
-            overflow: 'hidden'
-        },
-        appBarButtons: {
-            flex: '0 0 auto'
-        },
-        renamer: {
-            padding: 8
-        },
-        scrollingComponent: {
-            flex: 1,
-            padding: '8px 0',
-            overflow: 'auto'
-        },
-        nodeContent: {
-            height: '100%',
-            display: 'flex',
-            alignItems: 'center'
-        },
-        dragSource: {
-            padding: 8,
-            margin: -8
-        },
-        chip: {
-            minWidth: minTouchTargetSize,
-            fontSize: 14,
-            backgroundColor: 'transparent',
-            border: '1px solid ' + lineColor
-        },
-        functionChip: {
-            color: indigo[200]
-        },
-        variableChip: {
-            color: lightBlue[200]
-        },
-        referenceChip: {
-            color: grey[300]
-        },
-        numberChip: {
-            color: green[200]
-        },
-        placeholderChip: {
-            color: red[200]
-        },
-        lineChildren: {
-            position: 'absolute',
-            width: 1,
-            left: minTouchTargetSize / 2,
-            bottom: 0,
-            height: (minTouchTargetSize - chipHeight) / 2,
-            backgroundColor: lineColor
-        },
-        pickedUp: {
-            boxShadow: theme.shadows[8]
-        },
-        bottomBar: {
-            padding: '0 16px'
-        },
-        popover: {
-            padding: '8px 0'
-        },
-        editMenu: {
-            width: 450,
-            maxHeight: 'calc(100% - 72px)' // leave space to click outside to dismiss
-        }
-    };
-};
-
-const initialTreeData = [{ type: 'placeholder', value: '' }];
-
-const modes = { default: 'default', menu: 'menu', edit: 'edit', add: 'add' };
+const styles = ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%',
+        userSelect: 'none'
+    },
+    appBarLeft: {
+        flex: '1 1 auto',
+        minWidth: 0,
+        display: 'flex'
+    },
+    name: {
+        padding: '8px 20px',
+        overflow: 'hidden'
+    },
+    appBarButtons: {
+        flex: '0 0 auto'
+    },
+    renamer: {
+        padding: 8
+    },
+    scrollingComponent: {
+        flex: 1,
+        padding: '8px 0',
+        overflow: 'auto'
+    },
+    bottomBar: {
+        padding: '0 16px'
+    }
+});
 
 const ScrollingComponent = withScrolling('div');
 
-const preventDefault = event => {
-    event.preventDefault();
-};
-
 class ExpressionPage extends Component {
     state = {
-        renamer: { open: false },
-        mode: modes.default,
-        menu: {},
-        variables: [] // stored as state to avoid changing them before the menu animation finishes
+        renamer: { open: false }
     };
 
     undo = () => {
@@ -179,10 +96,7 @@ class ExpressionPage extends Component {
             {this.renderAppBar()}
             {this.renderScrollingContent()}
             {this.renderBottomBar()}
-            {this.renderPreview()}
             {this.renderRenamer()}
-            {this.renderMenu()}
-            {this.renderEditMenu()}
         </div>
     );
 
@@ -233,93 +147,16 @@ class ExpressionPage extends Component {
 
     renderScrollingContent = () => (
         <ScrollingComponent className={this.props.classes.scrollingComponent}>
-            {this.renderSortableTree()}
+            {this.renderTreeEditor()}
         </ScrollingComponent>
     );
 
-    renderSortableTree = () => (
-        <SortableTree
+    renderTreeEditor = () => (
+        <TreeEditor
+            expressionNames={this.props.expressions.map(expr => expr.name)}
             treeData={this.props.expression.treeDataHistory.present}
-            onChange={this.addToHistory}
-            rowHeight={minTouchTargetSize}
-            scaffoldBlockPxWidth={minTouchTargetSize}
-            nodeContentRenderer={this.renderNodeContent}
-            isVirtualized={false
-                /* disable virtualization because it can remove an input element
-                   (and the associated on-screen keyboard) */
-            }
-        />
+            setTreeData={this.addToHistory} />
     );
-
-    renderNodeContent = nodeRendererProps => (
-        <div className={this.props.classes.nodeContent}>
-            {nodeRendererProps.isDragging ? null : this.renderChip(nodeRendererProps)}
-            {this.hasChildren(nodeRendererProps.node) ? this.renderLineChildren() : null}
-        </div>
-    );
-
-    renderChip = ({ node, path, treeIndex, connectDragSource }) => {
-        let classes = {
-            root: this.props.classes.chip,
-            label: this.getChipLabelClass(node.type)
-        };
-        let label = this.getNodeLabel(node);
-        let handleClick = event => this.openMenu(node, path, treeIndex, event.currentTarget);
-        let chip = <Chip classes={classes} label={label} onClick={handleClick} />;
-        // the drag source is an anchor tag since it seems to cause a vibration on long press
-        return connectDragSource(
-            <a className={this.props.classes.dragSource} onContextMenu={preventDefault}>{chip}</a>
-        );
-    };
-
-    getChipLabelClass = type => {
-        switch (type) {
-            case 'function':
-                return this.props.classes.functionChip;
-            case 'variable':
-                return this.props.classes.variableChip;
-            case 'number':
-                return this.props.classes.numberChip;
-            case 'placeholder':
-                return this.props.classes.placeholderChip;
-            default:
-                return this.props.classes.referenceChip;
-        }
-    };
-
-    getNodeLabel = node => {
-        switch (node.type) {
-            case 'number':
-                return node.value.toString();
-            case 'expression':
-                return this.props.expressions[node.value].name;
-            case 'function':
-                return lambdaChar + node.value;
-            default:
-                return node.value;
-        }
-    };
-
-    openMenu = (node, path, treeIndex, anchorEl) => {
-        this.setState({
-            mode: modes.menu,
-            menu: { node, path, treeIndex, anchorEl }
-        });
-    };
-
-    hasChildren = node => node.children && node.children.length;
-
-    renderLineChildren = () => <div className={this.props.classes.lineChildren} />;
-
-    renderPreview = () => <Preview generator={this.generatePreview} />;
-
-    generatePreview = (type, item, style) => {
-        let classes = {
-            root: this.props.classes.chip + ' ' + this.props.classes.pickedUp
-        };
-        let label = this.getNodeLabel(item.node);
-        return <div style={style}><Chip classes={classes} label={label} /></div>;
-    };
 
     renderRenamer = () => (
         <Popover
@@ -371,139 +208,6 @@ class ExpressionPage extends Component {
         }
     };
 
-    renderMenu = () => (
-        <Menu anchorEl={this.state.menu.anchorEl}
-              open={this.state.mode === modes.menu}
-              onClose={this.closeMenu}
-              disableRestoreFocus>
-            {this.renderGoIfNeeded()}
-            <MenuItem onClick={this.initiateEdit}>Edit</MenuItem>
-            <MenuItem onClick={this.removeNode}>Delete</MenuItem>
-            <MenuItem onClick={this.initiateAdd}>Add child</MenuItem>
-        </Menu>
-    );
-
-    renderGoIfNeeded = () => {
-        let path = this.getLinkPathToMenuNode();
-        return path && (
-            <MenuItem component={Link} to={path} onClick={this.closeMenu}>
-                Go
-            </MenuItem>
-        );
-    };
-
-    getLinkPathToMenuNode = () => {
-        let { node } = this.state.menu;
-        if (!node) {
-            return null;
-        }
-        switch (node.type) {
-            case 'special':
-                return '/special-forms/' + encodeURIComponent(node.value);
-            case 'primitive':
-                return '/primitive-functions/' + encodeURIComponent(node.value);
-            case 'expression':
-                return '/expressions/' + node.value;
-            default:
-                return null;
-        }
-    };
-
-    initiateEdit = () => {
-        this.setState(state => ({
-            mode: modes.edit,
-            variables: this.getVariables()
-        }));
-    };
-
-    getVariables = () => {
-        let flatData = getFlatDataFromTree({
-            treeData: this.props.expression.treeDataHistory.present,
-            getNodeKey: ({ treeIndex }) => treeIndex
-        });
-        let nodes = flatData.map(({ node }) => node);
-        let functionNodes = nodes.filter(node => node.type === 'function');
-        return functionNodes.map(node => node.value);
-    };
-
-    removeNode = () => {
-        let resultTreeData = removeNodeAtPath({
-            treeData: this.props.expression.treeDataHistory.present,
-            path: this.state.menu.path,
-            getNodeKey: ({ treeIndex }) => treeIndex
-        });
-        let newTreeData = resultTreeData.length === 0 ? initialTreeData : resultTreeData;
-        this.addToHistory(newTreeData);
-        this.closeMenu();
-    };
-
-    initiateAdd = () => {
-        this.setState({
-            mode: modes.add,
-            variables: this.getVariables()
-        });
-    };
-
-    renderEditMenu = () => (
-        <Popover
-            classes={{ paper: `${this.props.classes.popover} ${this.props.classes.editMenu}` }}
-            anchorEl={this.state.menu.anchorEl}
-            anchorOrigin={{ vertical: -16 }}
-            open={this.state.mode === modes.edit || this.state.mode === modes.add}
-            onClose={this.closeMenu}>
-            {this.renderEditor()}
-        </Popover>
-    );
-
-    renderEditor = () => (
-        <NodeEditor
-            initialInput={this.getInitialEditorInput()}
-            items={this.getAllPossibleEditorItems()}
-            saveResult={this.saveEditorResult} />
-    );
-
-    getInitialEditorInput = () => this.state.mode === 'edit' ? this.getNodeLabel(this.state.menu.node) : '';
-
-    getAllPossibleEditorItems = () => [
-        ...this.state.variables.map(variable => ({ name: variable, type: 'variable', value: variable })),
-        ...Object.entries(specialForms).map(([name, info]) => ({ name, type: 'special', value: name })),
-        ...Object.entries(primitiveFunctions).map(([name, info]) => ({ name, type: 'primitive', value: name })),
-        ...this.props.expressions
-                .map((expression, index) => ({ name: expression.name, type: 'expression', value: index }))
-                .filter(item => item.name)
-    ];
-
-    saveEditorResult = (type, value) => {
-        let { mode, menu } = this.state;
-        let { treeDataHistory } = this.props.expression;
-        if (mode === 'edit') {
-            let valueChanged = type !== menu.node.type || value !== menu.node.value;
-            if (valueChanged) {
-                this.addToHistory(changeNodeAtPath({
-                    treeData: treeDataHistory.present,
-                    path: menu.path,
-                    newNode: { ...menu.node, type, value },
-                    getNodeKey: ({ treeIndex }) => treeIndex
-                }));
-            }
-        } else {
-            if (value !== '') {
-                this.addToHistory(addNodeUnderParent({
-                    treeData: treeDataHistory.present,
-                    parentKey: menu.treeIndex,
-                    expandParent: true,
-                    newNode: { type, value },
-                    getNodeKey: ({ treeIndex }) => treeIndex
-                }).treeData);
-            }
-        }
-        this.closeMenu();
-    };
-
-    closeMenu = () => {
-        this.setState({ mode: modes.default });
-    };
-
     renderBottomBar = () => (
         <Toolbar className={this.props.classes.bottomBar}>
             <Typography variant="subheading">
@@ -531,7 +235,7 @@ class ExpressionPage extends Component {
             name: "",
             treeDataHistory: {
                 past: [],
-                present: initialTreeData,
+                present: TreeEditor.INITIAL_TREE_DATA,
                 future: []
             }
         };
---
+++ src/TreeEditor.js
@@ -0,0 +1,320 @@
+import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { Preview } from 'react-dnd-multi-backend';
+import { SortableTreeWithoutDndContext as SortableTree, changeNodeAtPath, removeNodeAtPath, addNodeUnderParent, getFlatDataFromTree } from 'react-sortable-tree';
+import { withStyles } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
+import indigo from '@material-ui/core/colors/indigo';
+import lightBlue from '@material-ui/core/colors/lightBlue';
+import green from '@material-ui/core/colors/green';
+import grey from '@material-ui/core/colors/grey';
+import Chip from '@material-ui/core/Chip';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Popover from '@material-ui/core/Popover';
+import NodeEditor from './NodeEditor';
+import primitiveFunctions from './primitiveFunctions';
+import specialForms from './specialForms';
+
+const chipHeight = 32;
+const minTouchTargetSize = 48;
+const lambdaChar = '\u03BB';
+
+const styles = theme => {
+    let lineColor = theme.palette.grey[700];
+    let lineColorBackground = { backgroundColor: lineColor };
+    return {
+        '@global': {
+            '.rst__lineHalfHorizontalRight::before': lineColorBackground,
+            '.rst__lineFullVertical::after': lineColorBackground,
+            '.rst__lineHalfVerticalTop::after': lineColorBackground,
+            '.rst__lineHalfVerticalBottom::after': lineColorBackground
+        },
+        nodeContent: {
+            height: '100%',
+            display: 'flex',
+            alignItems: 'center'
+        },
+        dragSource: {
+            padding: 8,
+            margin: -8
+        },
+        chip: {
+            minWidth: minTouchTargetSize,
+            fontSize: 14,
+            backgroundColor: 'transparent',
+            border: '1px solid ' + lineColor
+        },
+        functionChip: {
+            color: indigo[200]
+        },
+        variableChip: {
+            color: lightBlue[200]
+        },
+        referenceChip: {
+            color: grey[300]
+        },
+        numberChip: {
+            color: green[200]
+        },
+        placeholderChip: {
+            color: red[200]
+        },
+        lineChildren: {
+            position: 'absolute',
+            width: 1,
+            left: minTouchTargetSize / 2,
+            bottom: 0,
+            height: (minTouchTargetSize - chipHeight) / 2,
+            backgroundColor: lineColor
+        },
+        pickedUp: {
+            boxShadow: theme.shadows[8]
+        },
+        editMenu: {
+            width: 450,
+            maxHeight: 'calc(100% - 72px)', // leave space to click outside to dismiss
+            padding: '8px 0'
+        }
+    };
+};
+
+const modes = { default: 'default', menu: 'menu', edit: 'edit', add: 'add' };
+
+class TreeEditor extends Component {
+    state = {
+        mode: modes.default,
+        menu: {},
+        variables: [] // stored as state to avoid changing them before the menu animation finishes
+    };
+
+    render = () => (
+        <Fragment>
+            {this.renderTree()}
+            {this.renderPreview()}
+            {this.renderMenu()}
+            {this.renderEditMenu()}
+        </Fragment>
+    );
+
+    renderTree = () => (
+        <SortableTree
+            treeData={this.props.treeData}
+            onChange={this.props.setTreeData}
+            rowHeight={minTouchTargetSize}
+            scaffoldBlockPxWidth={minTouchTargetSize}
+            nodeContentRenderer={this.renderNodeContent}
+            isVirtualized={false
+                /* disable virtualization because it can remove an input element
+                   (and the associated on-screen keyboard) */
+            }
+        />
+    );
+
+    renderNodeContent = nodeRendererProps => (
+        <div className={this.props.classes.nodeContent}>
+            {nodeRendererProps.isDragging ? null : this.renderChip(nodeRendererProps)}
+            {this.hasChildren(nodeRendererProps.node) ? this.renderLineChildren() : null}
+        </div>
+    );
+
+    renderChip = ({ node, path, treeIndex, connectDragSource }) => {
+        let classes = {
+            root: this.props.classes.chip,
+            label: this.getChipLabelClass(node.type)
+        };
+        let label = this.getNodeLabel(node);
+        let handleClick = event => this.openMenu(node, path, treeIndex, event.currentTarget);
+        let chip = <Chip classes={classes} label={label} onClick={handleClick} />;
+        // the drag source is an anchor tag since it seems to cause a vibration on long press
+        return connectDragSource(
+            <a className={this.props.classes.dragSource} onContextMenu={e => e.preventDefault()}>{chip}</a>
+        );
+    };
+
+    getChipLabelClass = type => {
+        switch (type) {
+            case 'function':
+                return this.props.classes.functionChip;
+            case 'variable':
+                return this.props.classes.variableChip;
+            case 'number':
+                return this.props.classes.numberChip;
+            case 'placeholder':
+                return this.props.classes.placeholderChip;
+            default:
+                return this.props.classes.referenceChip;
+        }
+    };
+
+    getNodeLabel = node => {
+        switch (node.type) {
+            case 'number':
+                return node.value.toString();
+            case 'expression':
+                return this.props.expressionNames[node.value];
+            case 'function':
+                return lambdaChar + node.value;
+            default:
+                return node.value;
+        }
+    };
+
+    openMenu = (node, path, treeIndex, anchorEl) => {
+        this.setState({
+            mode: modes.menu,
+            menu: { node, path, treeIndex, anchorEl }
+        });
+    };
+
+    hasChildren = node => node.children && node.children.length;
+
+    renderLineChildren = () => <div className={this.props.classes.lineChildren} />;
+
+    renderPreview = () => <Preview generator={this.generatePreview} />;
+
+    generatePreview = (type, item, style) => {
+        let classes = {
+            root: this.props.classes.chip + ' ' + this.props.classes.pickedUp
+        };
+        let label = this.getNodeLabel(item.node);
+        return <div style={style}><Chip classes={classes} label={label} /></div>;
+    };
+
+    renderMenu = () => (
+        <Menu anchorEl={this.state.menu.anchorEl}
+              open={this.state.mode === modes.menu}
+              onClose={this.closeMenu}
+              disableRestoreFocus>
+            {this.renderGoIfNeeded()}
+            <MenuItem onClick={this.initiateEdit}>Edit</MenuItem>
+            <MenuItem onClick={this.removeNode}>Delete</MenuItem>
+            <MenuItem onClick={this.initiateAdd}>Add child</MenuItem>
+        </Menu>
+    );
+
+    renderGoIfNeeded = () => {
+        let path = this.getLinkPathToMenuNode();
+        return path && (
+            <MenuItem component={Link} to={path} onClick={this.closeMenu}>
+                Go
+            </MenuItem>
+        );
+    };
+
+    getLinkPathToMenuNode = () => {
+        let { node } = this.state.menu;
+        if (!node) {
+            return null;
+        }
+        switch (node.type) {
+            case 'special':
+                return '/special-forms/' + encodeURIComponent(node.value);
+            case 'primitive':
+                return '/primitive-functions/' + encodeURIComponent(node.value);
+            case 'expression':
+                return '/expressions/' + node.value;
+            default:
+                return null;
+        }
+    };
+
+    initiateEdit = () => {
+        this.setState(state => ({
+            mode: modes.edit,
+            variables: this.getVariables()
+        }));
+    };
+
+    getVariables = () => {
+        let flatData = getFlatDataFromTree({
+            treeData: this.props.treeData,
+            getNodeKey: ({ treeIndex }) => treeIndex
+        });
+        let nodes = flatData.map(({ node }) => node);
+        let functionNodes = nodes.filter(node => node.type === 'function');
+        return functionNodes.map(node => node.value);
+    };
+
+    removeNode = () => {
+        let resultTreeData = removeNodeAtPath({
+            treeData: this.props.treeData,
+            path: this.state.menu.path,
+            getNodeKey: ({ treeIndex }) => treeIndex
+        });
+        let newTreeData = resultTreeData.length === 0 ? TreeEditor.INITIAL_TREE_DATA : resultTreeData;
+        this.props.setTreeData(newTreeData);
+        this.closeMenu();
+    };
+
+    initiateAdd = () => {
+        this.setState({
+            mode: modes.add,
+            variables: this.getVariables()
+        });
+    };
+
+    renderEditMenu = () => (
+        <Popover
+            classes={{ paper: `${this.props.classes.popover} ${this.props.classes.editMenu}` }}
+            anchorEl={this.state.menu.anchorEl}
+            anchorOrigin={{ vertical: -16 }}
+            open={this.state.mode === modes.edit || this.state.mode === modes.add}
+            onClose={this.closeMenu}>
+            {this.renderNodeEditor()}
+        </Popover>
+    );
+
+    renderNodeEditor = () => (
+        <NodeEditor
+            initialInput={this.getInitialEditorInput()}
+            items={this.getAllPossibleEditorItems()}
+            saveResult={this.saveEditorResult} />
+    );
+
+    getInitialEditorInput = () => this.state.mode === 'edit' ? this.getNodeLabel(this.state.menu.node) : '';
+
+    getAllPossibleEditorItems = () => [
+        ...this.state.variables.map(variable => ({ name: variable, type: 'variable', value: variable })),
+        ...Object.entries(specialForms).map(([name, info]) => ({ name, type: 'special', value: name })),
+        ...Object.entries(primitiveFunctions).map(([name, info]) => ({ name, type: 'primitive', value: name })),
+        ...this.props.expressionNames
+                .map((name, index) => ({ name, type: 'expression', value: index }))
+                .filter(item => item.name)
+    ];
+
+    saveEditorResult = (type, value) => {
+        let { mode, menu } = this.state;
+        let { treeData } = this.props;
+        if (mode === 'edit') {
+            let valueChanged = type !== menu.node.type || value !== menu.node.value;
+            if (valueChanged) {
+                this.props.setTreeData(changeNodeAtPath({
+                    treeData,
+                    path: menu.path,
+                    newNode: { ...menu.node, type, value },
+                    getNodeKey: ({ treeIndex }) => treeIndex
+                }));
+            }
+        } else {
+            if (value !== '') {
+                this.props.setTreeData(addNodeUnderParent({
+                    treeData,
+                    parentKey: menu.treeIndex,
+                    expandParent: true,
+                    newNode: { type, value },
+                    getNodeKey: ({ treeIndex }) => treeIndex
+                }).treeData);
+            }
+        }
+        this.closeMenu();
+    };
+
+    closeMenu = () => {
+        this.setState({ mode: modes.default });
+    };
+
+    static INITIAL_TREE_DATA = [{ type: 'placeholder', value: '' }];
+}
+
+export default withStyles(styles)(TreeEditor);
